fac8f8f3289f5ded8e426cb495e0813a13490401
Daniel Barbosa Bezerra <bbarbosa.daniel@gmail.com>
2026-05-10 21:37:31 -0300

    feat: initial Spring Booot setup

---
+++ projeto-biblioteca/src/test/java/com/daniel/projeto_biblioteca/ProjetoBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.projeto_biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
