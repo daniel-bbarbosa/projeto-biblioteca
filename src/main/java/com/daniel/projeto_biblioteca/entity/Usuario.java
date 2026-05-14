309c8ace58be2483dc58d62d3830b9fa684ffdbb
wechileysniper17-arch <wechileysniper17@gmail.com>
2026-05-14 19:25:43 -0300

    Create Usuario.java

---
+++ src/main/java/com/daniel/projeto_biblioteca/entity/Usuario.java
@@ -0,0 +1,21 @@
+package com.Daniel.projeto_biblioteca.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String telefone;
+}
